57dbee3e4547732906e91f75fe70301488d7159c
Rahul G <rahulg.github@gmail.com>
2023-07-19 19:12:54 +0530

    keybind changed

--- config.def.h
+++ config.def.h
@@ -67,7 +67,7 @@ static const char *termcmd[]  = { "st", NULL };
 #include "shift-tools.c"
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_space,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -88,8 +88,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	/*{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },*/
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_p,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_p,  togglefloating, {0} },
 	{ MODKEY,                       XK_g,      gesture,        {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
